45c1a71b799b342954f2894a012a5fd2b6062773
Anshuj <anshuj@gmail.com>
2019-08-17 08:17:18 -0700

    Made a clickCounter in react

---
+++ clickCounter/frontend/click_counter.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+class ClickCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      prevClk: []
+    };
+  }
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({ count: this.state.count + 1 });
+  }
+  handleReset(e) {
+    e.preventDefault();
+    const prevClk = this.state.prevClk;
+    prevClk.push(this.state.count);
+    this.setState({ count: 0, prevClk });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick.bind(this)}>Click Me!</button>
+        <div>{this.state.count}</div>
+        <button onClick={this.handleReset.bind(this)}>Reset</button>
+            <div>{this.state.prevClk[0]}</div>
+      </div>
+    );
+  }
+}
+
+export default ClickCounter;
